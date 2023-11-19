e75477d3702719d898c38fcf0b2294c5abf90a36
Anuj Bhati <anujbhati7488@gmail.com>
2023-11-19 11:59:07 +0530

    Added timestamps in the notification modal

--- models/notificationModal.js
+++ models/notificationModal.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
-const notificationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Types.ObjectId, ref: 'User' },
-  title: { type: String, required: true },
-  description: { type: String, unique: true },
-});
+const notificationSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Types.ObjectId, ref: 'User' },
+    title: { type: String, required: true },
+    description: { type: String, unique: true },
+  },
+  { timestamps: true }
+);
 
 const Notification = mongoose.model('Notification', notificationSchema);
 
